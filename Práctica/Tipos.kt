c84744c023f48ad2198b40e9a36981f432934307
FerlyGuti_0002 <ferlyquiroz17@gmail.com>
2020-11-25 20:33:22 -0600

    Práctica2 -Ejercicios_Kotlin

---
+++ Práctica/Tipos.kt
@@ -0,0 +1,26 @@
+/*fun main()
+{
+    val int = 123
+    val long = 123456L
+    val double = 12.34
+    val float = 12.34F
+    val hexadecimal = 0xAB
+    val binary = 0b01010101
+    println("Esta es una variable entera $int Esta es una variable long $long Este es un double $double Esta es una variable de tipo float $float Esta es un valor hexadecimal $hexadecimal Este es un valor binario $binary")
+}*/
+
+/*fun main()
+{
+    val x = 1 
+    val y = 2 
+    val z = 2 
+    val esTrue = x < y && x < z 
+    val otroTrue = x == y || y == z
+    println("$x $y $z $esTrue $otroTrue")
+}*/
+
+fun main()
+{
+    
+}
+
---
+++ Práctica/Variables.kt
@@ -0,0 +1,26 @@
+/*Captura1
+fun main()
+{
+    var nombre = "Ferly"
+    println(nombre)
+}*/
+
+/*Captura2
+ fun main()
+ {
+    val apellido = "Quiroz Gutiérrez" 
+//  println(apellido)
+ }*/
+
+//Captura3
+ fun main()
+ {
+    var nombre = "Ferly"
+    val apellido = "Quiroz Gutiérrez" 
+    val edad = "20 años"
+    println("Mi nombre es $nombre $apellido y tengo $edad años")
+ }
+
+
+
+
